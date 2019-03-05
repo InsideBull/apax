2f75e613d283579af8e2e3aa2c8f528157bb76ac
andry <andriamisainacontact@gmail.com>
2019-03-05 10:06:28 +0300

    config

---
+++ wp-content/plugins-2018/adaka-export-content/adaka-export-content.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Adaka Export Content
+Version: 1.0.0
+Author: ADaKa
+Author URI: http://www.adaka.fr
+*/
+
+
+require_once 'includes/post_submit.php';
+require_once 'includes/export-plugin.php';
+
+// menu item
+function a_export_register_admin_page() {
+	add_menu_page('Exports des contenus', 'Exports', 'read', 'a_export_content', "a_display_form", 'dashicons-download', 50);
+}
+add_action('admin_menu', 'a_export_register_admin_page');
